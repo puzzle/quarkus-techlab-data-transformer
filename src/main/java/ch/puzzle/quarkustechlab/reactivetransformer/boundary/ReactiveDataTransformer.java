3fec4560e28ae9153fd2742be26b303c3deab231
Christian Schlatter <schlatter@puzzle.ch>
2021-02-16 09:51:10 +0100

    fix jaeger traces, addfeature toggle

--- src/main/java/ch/puzzle/quarkustechlab/reactivetransformer/boundary/ReactiveDataTransformer.java
+++ src/main/java/ch/puzzle/quarkustechlab/reactivetransformer/boundary/ReactiveDataTransformer.java
@@ -7,6 +7,7 @@ import io.opentracing.SpanContext;
 import io.opentracing.Tracer;
 import io.opentracing.propagation.Format;
 import io.smallrye.reactive.messaging.kafka.IncomingKafkaRecordMetadata;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.metrics.MetricUnits;
 import org.eclipse.microprofile.metrics.annotation.Counted;
 import org.eclipse.microprofile.metrics.annotation.Timed;
@@ -31,22 +32,34 @@ public class ReactiveDataTransformer {
     @Inject
     Tracer tracer;
 
+    @ConfigProperty(name = "transformer.jaeger.enabled")
+    Optional<Boolean> jaegerEnabled;
 
     @Counted(name = "messagesTransformed", description = "How many messages were transformed.")
     @Incoming("data")
     @Timed(name = "transformationTimer", description = "A measure of how long it takes to transform the data.", unit = MetricUnits.MILLISECONDS)
     public CompletionStage<Void> consumeStream(Message<SensorMeasurement> message) {
-        logger.info("Message received");
+
         Optional<IncomingKafkaRecordMetadata> metadata = message.getMetadata(IncomingKafkaRecordMetadata.class);
         if (metadata.isPresent()) {
-            SpanContext extract = tracer.extract(Format.Builtin.TEXT_MAP, new HeadersMapExtractAdapter(metadata.get().getHeaders()));
-            try (Scope scope = tracer.buildSpan("transform-data").asChildOf(extract).startActive(true)) {
-                sum += message.getPayload().data;
-                count++;
-                logger.info("Current average: " + sum / count);
-                return message.ack();
+            if(jaegerEnabled.orElse(false)){
+                SpanContext extract = tracer.extract(Format.Builtin.TEXT_MAP, new HeadersMapExtractAdapter(metadata.get().getHeaders()));
+                try (Scope scope = tracer.buildSpan("transform-data").asChildOf(extract).startActive(true)) {
+                    logger.info("Received reactive message with jaeger metadata");
+                    return processMessage(message);
+                }
+            }else {
+                logger.info("Received reactive message");
+                return processMessage(message);
             }
         }
         return message.nack(new RuntimeException());
+    }
+
+    private CompletionStage<Void> processMessage(Message<SensorMeasurement> message){
+        sum += message.getPayload().data;
+        count++;
+        logger.info("Current average: " + sum / count);
+        return message.ack();
     }
 }
